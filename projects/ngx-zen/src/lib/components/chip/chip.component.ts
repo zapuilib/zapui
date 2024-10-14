df5310c4e788de27b7cf2cc63092ec1280cce217
Bikas KC <bikas@87-224-45-153.spitfireuk.net>
2024-10-14 10:51:37 +0100

    Chips updated

--- projects/ngx-zen/src/lib/components/chip/chip.component.ts
+++ projects/ngx-zen/src/lib/components/chip/chip.component.ts
@@ -1,9 +1,8 @@
 import { Component, Inject, Input, Output, EventEmitter } from '@angular/core';
+
 import { NGX_ZEN_CONFIG } from '../../tokens/ngx-zen.tokens';
 import { NgxZenConfig } from '../../interfaces/config.interface';
 import { Styles } from '../../interfaces/style.interface';
-
-
 
 @Component({
   selector: 'ngx-zen-chip',
@@ -12,14 +11,14 @@ import { Styles } from '../../interfaces/style.interface';
 })
 export class ChipComponent {
   @Output() removed = new EventEmitter<void>();
-  @Input() text = '';
+  @Input() text = 'Chip';
   @Input() zenClass: string = '';
   @Input() variant: 'outlined' | 'default' = 'default';
   @Input() size: 'small' | 'wide' | 'wider' = 'wide';
   @Input() icon?: string;
   @Input() iconPosition: 'left' | 'right' = 'left';
   @Input() disabled = false;
-  @Input() removable = false;
+  @Input() dismissible = false;
 
   constructor(@Inject(NGX_ZEN_CONFIG) private config: NgxZenConfig) {}
 
@@ -27,14 +26,31 @@ export class ChipComponent {
     const isOutlined = this.variant === 'outlined';
     return {
       backgroundColor: isOutlined ? 'transparent' : this.config.colors.tertiary,
-       color: isOutlined ? this.config.colors.info : this.config.colors.primary, 
-      fontSize: this.size === 'small' ? this.config.fontSize.xs : this.size === 'wide' ? this.config.fontSize.sm : this.config.fontSize.md,
+      color: isOutlined
+        ? this.config.colors.tertiary
+        : this.config.colors.primary,
+      borderColor: isOutlined ? this.config.colors.tertiary : 'transparent',
+      fontSize:
+        this.size === 'small'
+          ? this.config.fontSize.xs
+          : this.size === 'wide'
+          ? this.config.fontSize.sm
+          : this.config.fontSize.md,
+    };
+  }
+
+  getTextStyle(): Styles {
+    const isOutlined = this.variant === 'outlined';
+    return {
+      color: isOutlined
+        ? this.config.colors.tertiary
+        : this.config.colors.primary,
     };
   }
 
   onRemove(event: Event) {
     event.stopPropagation();
-    if (!this.disabled && this.removable) {
+    if (!this.disabled && this.dismissible) {
       this.removed.emit();
     }
   }

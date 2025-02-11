c6ca297f8db1ec1f96b4e3e07cd088d38cd17742
akabikas <akabikaskc@gmail.com>
2025-02-11 17:54:34 +0000

    Button icon directive wip styling

--- projects/zap/core/button/button.component.ts
+++ projects/zap/core/button/button.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ContentChild, Input, ChangeDetectorRef } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input } from '@angular/core';
+
 import { ZapButtonIconDirective } from './button-icon.directive';
 
 @Component({
@@ -9,12 +10,13 @@ import { ZapButtonIconDirective } from './button-icon.directive';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ZapButtonComponent implements AfterViewInit {
+export class ZapButtonComponent implements AfterContentInit {
   @Input() text = 'Submit';
   @Input() zapClass: string = '';
   @Input() shape!: 'pill' | 'curve' | 'flat';
   @Input() size!: 'compact' | 'wide' | 'tight' | 'base';
-  @Input() type: 'info' | 'success' | 'warning' | 'error' | 'default' = 'default';
+  @Input() type: 'info' | 'success' | 'warning' | 'error' | 'default' =
+    'default';
   @Input() icon!: string;
   @Input() iconPosition: 'left' | 'right' = 'left';
   @Input() img: string | undefined = undefined;
@@ -23,15 +25,12 @@ export class ZapButtonComponent implements AfterViewInit {
   @Input() disabled: boolean = false;
   @Input() icononly: boolean = false;
 
-  @ContentChild(ZapButtonIconDirective, { static: false })
-  iconDirective?: ZapButtonIconDirective;
+  @ContentChild(ZapButtonIconDirective, { static: false }) iconDirective?: ZapButtonIconDirective;
 
-  constructor(private cdr: ChangeDetectorRef) {}
-
-  ngAfterViewInit() {
-    this.cdr.detectChanges();
-    console.log('iconDirective:', this.iconDirective);
+  ngAfterContentInit() {
+    console.log(this.iconDirective);
     if (this.iconDirective) {
+      
       const iconElement = this.iconDirective.el.nativeElement;
       iconElement.classList.add('__zap__button__icon');
     }

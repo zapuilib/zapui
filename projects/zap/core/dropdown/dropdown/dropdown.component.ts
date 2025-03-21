a172fa48f83c8a3303203390302dd425eb2689a9
Bikas KC <bikas@togethercreative.co.uk>
2025-03-21 13:49:05 +0000

    feat/cdk: angualr cdk utilisation concept

--- projects/zap/core/dropdown/dropdown/dropdown.component.ts
+++ projects/zap/core/dropdown/dropdown/dropdown.component.ts
@@ -1,7 +1,13 @@
-import { Component, ContentChild } from '@angular/core';
-
-import { ZapDropdownMenu } from '../dropdown-menu/dropdown-menu.component';
-import { ZapDropdownTrigger } from '../dropdown-trigger/dropdown-trigger.component';
+import {
+  Component,
+  ViewChild,
+  TemplateRef,
+  ViewContainerRef,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { Overlay, OverlayRef, OverlayPositionBuilder } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'zap-dropdown',
@@ -9,26 +15,77 @@ import { ZapDropdownTrigger } from '../dropdown-trigger/dropdown-trigger.compone
   imports: [],
   template: `
     <div class="relative">
-      <div>
+      <!-- Trigger -->
+      <div
+        #triggerRef
+        (click)="toggleMenu()"
+        (keydown.enter)="toggleMenu()"
+        (keydown.space)="toggleMenu()"
+        tabindex="0"
+        role="button"
+        aria-haspopup="true"
+        [attr.aria-expanded]="overlayRef?.hasAttached()">
         <ng-content select="zap-dropdown-trigger"></ng-content>
       </div>
-      <div [style.display]="isMenuOpen ? 'block' : 'none'">
+
+      <!-- Menu template -->
+      <ng-template #menuTemplate>
         <ng-content select="zap-dropdown-menu"></ng-content>
-      </div>
+      </ng-template>
     </div>
   `,
-  styleUrl: './dropdown.component.scss',
+  styleUrls: ['./dropdown.component.scss'],
 })
-export class ZapDropdown {
-  @ContentChild(ZapDropdownMenu) menu!: ZapDropdownMenu;
-  @ContentChild(ZapDropdownTrigger) trigger!: ZapDropdownTrigger;
-  isMenuOpen = false;
+export class ZapDropdown implements AfterViewInit {
+  @ViewChild('triggerRef', { read: ElementRef }) triggerRef!: ElementRef;
+  @ViewChild('menuTemplate') menuTemplate!: TemplateRef<any>;
 
-  private adjustPosition() {
-    if (typeof window === 'undefined') return;
+  overlayRef!: OverlayRef;
+
+  constructor(
+    private overlay: Overlay,
+    private vcr: ViewContainerRef,
+    private positionBuilder: OverlayPositionBuilder,
+  ) {}
+
+  ngAfterViewInit(): void {
+    const positionStrategy = this.positionBuilder
+      .flexibleConnectedTo(this.triggerRef)
+      .withPositions([
+        {
+          originX: 'center',
+          originY: 'bottom',
+          overlayX: 'center',
+          overlayY: 'top',
+        },
+        {
+          originX: 'center',
+          originY: 'top',
+          overlayX: 'center',
+          overlayY: 'bottom',
+        },
+      ])
+      .withFlexibleDimensions(false)
+      .withPush(false);
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy: this.overlay.scrollStrategies.reposition(),
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+    });
+
+    this.overlayRef.backdropClick().subscribe(() => {
+      this.overlayRef.detach();
+    });
   }
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+    if (this.overlayRef.hasAttached()) {
+      this.overlayRef.detach();
+    } else {
+      const portal = new TemplatePortal(this.menuTemplate, this.vcr);
+      this.overlayRef.attach(portal);
+    }
   }
 }

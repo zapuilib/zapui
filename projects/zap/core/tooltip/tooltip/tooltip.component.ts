c1fb91be40218634958acc3609628996b0fe152c
Bikas KC <bikas@togethercreative.co.uk>
2025-03-21 15:35:51 +0000

    feat/tooltip: custom positioning with angular cdk

--- projects/zap/core/tooltip/tooltip/tooltip.component.ts
+++ projects/zap/core/tooltip/tooltip/tooltip.component.ts
@@ -1,17 +1,165 @@
-import { Component, Input } from '@angular/core';
+import { Overlay, OverlayPositionBuilder, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import {
+  Component,
+  ElementRef,
+  Input,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'zap-tooltip',
   standalone: true,
   template: `
-    <div #tooltip class="zap__tooltip">
-      <ng-content select="zap-tooltip-handler"></ng-content>
-      <ng-content select="zap-tooltip-content"></ng-content>
+    <div class="zap__tooltip">
+      <div #triggerRef (mouseenter)="showContent()" (mouseleave)="hideContent()">
+        <ng-content select="zap-tooltip-handler"></ng-content>
+      </div>
+      <ng-template #contentTemplate>
+        <ng-content select="zap-tooltip-content"></ng-content>
+      </ng-template>
     </div>
   `,
   styleUrls: ['./tooltip.component.scss'],
 })
 export class ZapTooltip {
+  @ViewChild('triggerRef', { read: ElementRef }) triggerRef!: ElementRef;
+  @ViewChild('contentTemplate') contentTemplate!: TemplateRef<any>;
   @Input() shape: 'curve' | 'pill' | 'flat' = 'flat';
   @Input() position: 'top' | 'bottom' | 'left' | 'right' | 'auto' = 'auto';
+
+  overlayRef!: OverlayRef;
+
+  constructor(
+    private overlay: Overlay,
+    private vcr: ViewContainerRef,
+    private positionBuilder: OverlayPositionBuilder,
+  ) {}
+
+  ngAfterViewInit(): void {
+    const positionConfigs = {
+      bottom: [
+        {
+          originX: 'center',
+          originY: 'bottom',
+          overlayX: 'center',
+          overlayY: 'top',
+          offsetY: 4,
+        },
+      ],
+      top: [
+        {
+          originX: 'center',
+          originY: 'top',
+          overlayX: 'center',
+          overlayY: 'bottom',
+          offsetY: -4,
+        },
+      ],
+      right: [
+        {
+          originX: 'end',
+          originY: 'center',
+          overlayX: 'start',
+          overlayY: 'center',
+          offsetX: 8,
+        },
+      ],
+      left: [
+        {
+          originX: 'start',
+          originY: 'center',
+          overlayX: 'end',
+          overlayY: 'center',
+          offsetX: -8,
+        },
+      ],
+      auto: [
+        {
+          originX: 'center',
+          originY: 'top',
+          overlayX: 'center',
+          overlayY: 'bottom',
+          offsetY: -4,
+        },
+        {
+          originX: 'center',
+          originY: 'bottom',
+          overlayX: 'center',
+          overlayY: 'top',
+          offsetY: 4,
+        },
+        {
+          originX: 'end',
+          originY: 'center',
+          overlayX: 'start',
+          overlayY: 'center',
+          offsetX: 4,
+        },
+        {
+          originX: 'start',
+          originY: 'center',
+          overlayX: 'end',
+          overlayY: 'center',
+          offsetX: -4,
+        },
+      ],
+    };
+
+    const positions = positionConfigs[this.position];
+
+    const positionStrategy = this.positionBuilder
+      .flexibleConnectedTo(this.triggerRef)
+      .withPositions(positions as any[])
+      .withFlexibleDimensions(false)
+      .withPush(false);
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy: this.overlay.scrollStrategies.reposition(),
+      panelClass: ['zap-tooltip-panel'],
+      hasBackdrop: false,
+    });
+  }
+
+  private hideTimeout: any;
+
+  showContent() {
+    if (this.hideTimeout) {
+      clearTimeout(this.hideTimeout);
+      this.hideTimeout = null;
+    }
+
+    if (!this.overlayRef.hasAttached()) {
+      const portal = new TemplatePortal(this.contentTemplate, this.vcr);
+      this.overlayRef.attach(portal);
+
+      this.overlayRef.overlayElement.addEventListener('mouseenter', () => {
+        if (this.hideTimeout) {
+          clearTimeout(this.hideTimeout);
+          this.hideTimeout = null;
+        }
+      });
+
+      this.overlayRef.overlayElement.addEventListener('mouseleave', () => this.hideContent());
+    }
+    this.overlayRef.overlayElement.classList.add('zap-tooltip-visible');
+  }
+
+  hideContent() {
+    if (this.overlayRef.hasAttached()) {
+      this.hideTimeout = setTimeout(() => {
+        this.overlayRef.overlayElement.classList.remove('zap-tooltip-visible');
+        this.overlayRef.detach();
+      }, 300);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
+    }
+  }
 }

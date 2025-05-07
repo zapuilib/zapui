a111cd280555b757a01ecca516111edb7f6d505c
Bikas KC <bikas@togethercreative.co.uk>
2025-05-07 15:00:11 +0100

    refactor/select: traditional @input and @output decorators updated to input and output signals

--- projects/demo/src/app/components/select/select.component.ts
+++ projects/demo/src/app/components/select/select.component.ts
@@ -29,4 +29,43 @@ export class SelectComponent {
     { label: 'Peru', value: 'pe' },
     { label: 'Other', value: 'other' },
   ];
+
+  updateCountries(query: string) {
+    const countries = [
+      { label: 'Australia', value: 'au' },
+      { label: 'New Zealand', value: 'nz' },
+      { label: 'Fiji', value: 'fj' },
+      { label: 'Papua New Guinea', value: 'pg' },
+      { label: 'Samoa', value: 'ws' },
+      { label: 'Tonga', value: 'to' },
+      { label: 'Solomon Islands', value: 'sb' },
+      { label: 'Vanuatu', value: 'vu' },
+      { label: 'Kiribati', value: 'ki' },
+      { label: 'Tuvalu', value: 'tv' },
+      { label: 'US', value: 'us' },
+      { label: 'Canada', value: 'ca' },
+      { label: 'Mexico', value: 'mx' },
+      { label: 'Brazil', value: 'br' },
+      { label: 'Colombia', value: 'co' },
+      { label: 'Argentina', value: 'ar' },
+      { label: 'Chile', value: 'cl' },
+      { label: 'Uruguay', value: 'uy' },
+      { label: 'Venezuela', value: 've' },
+      { label: 'Peru', value: 'pe' },
+      { label: 'Other', value: 'other' },
+    ];
+
+    setTimeout(() => {
+      if (query === '') {
+        this.countries = countries;
+      } else {
+        this.countries = countries.filter((country) =>
+          country.label.toLowerCase().includes(query.toLowerCase()),
+        );
+      }
+    }, 100);
+    if (this.countries.length === 0) {
+      this.countries = [{ label: 'No results found', value: '' }];
+    }
+  }
 }

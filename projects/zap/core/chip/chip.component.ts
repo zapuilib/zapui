cd18423bb00b24dd86c4abfe51d0f44d7d1c56a2
Bikas KC <bikas@togethercreative.co.uk>
2025-05-08 10:17:48 +0100

    refactor/chip: traditional @input and @output decorators updated to input and output signals

--- projects/zap/core/chip/chip.component.ts
+++ projects/zap/core/chip/chip.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter, ContentChild, AfterViewInit } from '@angular/core';
+import { Component, ContentChild, AfterViewInit, input, output } from '@angular/core';
 import { ZapIconDirective } from '../public-api';
 
 @Component({
@@ -9,50 +9,50 @@ import { ZapIconDirective } from '../public-api';
   styleUrls: ['./chip.component.scss'],
 })
 export class ZapChip implements AfterViewInit {
-  @Output() dismiss = new EventEmitter<void>();
-  @Input() text = 'Chip';
-  @Input() zapClass = '';
-  @Input() variant: 'outlined' | 'default' = 'default';
-  @Input() shape!: 'pill' | 'curve' | 'flat';
-  @Input() size: 'base' | 'compact' | 'wide' = 'base';
-  @Input() type: 'default' | 'info' | 'success' | 'warning' | 'error' = 'default';
-  @Input() icon?: string;
-  @Input() iconPosition: 'left' | 'right' = 'left';
-  @Input() disabled = false;
-  @Input() dismissible = false;
   @ContentChild(ZapIconDirective, { static: false })
   iconDirective!: ZapIconDirective;
+  dismiss = output();
+  text = input('Chip');
+  zapClass = input('');
+  variant = input<'outlined' | 'default'>('default');
+  shape = input<'pill' | 'curve' | 'flat'>();
+  size = input<'base' | 'compact' | 'wide'>('base');
+  type = input<'default' | 'info' | 'success' | 'warning' | 'error'>('default');
+  icon = input<string | undefined>(undefined);
+  iconPosition = input<'left' | 'right'>('left');
+  disabled = input(false);
+  dismissible = input(false);
 
   ngAfterViewInit() {
     if (this.iconDirective) {
       this.iconDirective.el.nativeElement.style.height = 'var(--zap-chip-font-size)';
       this.iconDirective.el.nativeElement.style.marginRight =
-        this.iconPosition === 'left' ? '8px' : '0';
+        this.iconPosition() === 'left' ? '8px' : '0';
       this.iconDirective.el.nativeElement.style.marginLeft =
-        this.iconPosition === 'right' ? '8px' : '0';
-      this.iconDirective.el.nativeElement.style.order = this.iconPosition === 'right' ? '1' : '0';
+        this.iconPosition() === 'right' ? '8px' : '0';
+      this.iconDirective.el.nativeElement.style.order = this.iconPosition() === 'right' ? '1' : '0';
     }
   }
 
   onRemove(event: Event) {
     event.stopPropagation();
-    if (!this.disabled && this.dismissible) {
+    if (!this.disabled() && this.dismissible()) {
       this.dismiss.emit();
     }
   }
 
   get classes(): string[] {
     return [
-      this.type,
-      this.shape,
-      this.size,
-      this.variant,
-      this.zapClass,
-      this.disabled ? 'disabled' : '',
-      this.dismissible ? 'dismissible' : '',
-      this.dismissible && this.icon && this.iconPosition === 'left'
+      this.type(),
+      this.shape() ?? '',
+      this.size(),
+      this.variant(),
+      this.zapClass(),
+      this.disabled() ? 'disabled' : '',
+      this.dismissible() ? 'dismissible' : '',
+      this.dismissible() && this.icon() && this.iconPosition() === 'left'
         ? 'dismissible-left'
-        : this.dismissible && this.icon && this.iconPosition === 'right'
+        : this.dismissible() && this.icon() && this.iconPosition() === 'right'
           ? 'dismissible-right'
           : '',
     ].filter((cls) => cls && cls !== 'default');

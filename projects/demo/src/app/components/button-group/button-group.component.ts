f464c4108b1894a143313f8cf5a53277f623febd
Bikas KC <bikas@togethercreative.co.uk>
2025-12-15 10:38:59 +0000

    refactor/button-group: icon replaced with svg icon on demo app

--- projects/demo/src/app/components/button-group/button-group.component.ts
+++ projects/demo/src/app/components/button-group/button-group.component.ts
@@ -10,6 +10,7 @@ import {
   ZapDropdownMenuShortcut,
   ZapDropdownSeparator,
   ZapDropdownTrigger,
+  ZapIconDirective,
 } from 'zap';
 
 @Component({
@@ -25,6 +26,7 @@ import {
     ZapDropdownMenuLabel,
     ZapDropdownMenuShortcut,
     ZapDropdownSeparator,
+    ZapIconDirective,
   ],
   templateUrl: './button-group.component.html',
   styleUrl: './button-group.component.scss',
